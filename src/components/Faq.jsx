52905df60a219107c03b21108c1a6409f5dd1bce
PriyankeshGithub <priyankeshshivam@gmail.com>
2023-04-10 23:56:41 +0300

    new updates and beautification

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React from 'react';
 import '../components/FaqStyle.css';
+import { useState } from 'react';
 
 const Faq = () => {
-  return (
-    <div className='faq'>Faq</div>
-  )
-}
+  const [text, setText] = useState(new Array(data.length).fill(false));
 
-export default Faq
+  const display = (index) => {
+    const newText = [...text];
+    newText[index] = !newText[index];
+    setText(newText);
+  };
+
+  return (
+    <div className='faq'>
+      <div className='accordion'>
+        {data.map((item, i) => (
+          <div className='item' key={i}>
+            <div className='tittle'>
+              <h2 onClick={() => display(i)}>{item.question}</h2>
+              <p onClick={() => display(i)}>{text[i] ? '-' : '+'}</p>
+            </div>
+            <div className='content'>{text[i] && item.answer}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const data = [
+  {
+    question: 'What kind of language courses do you offer?',
+    answer:
+      'We offer comprehensive language courses in Hindi, Lithuanian, and Arabic. Our courses are designed for both beginners and advanced learners, and we offer personalized lessons tailored to your unique needs and learning style.',
+  },
+  {
+    question: 'What materials do you use in your courses?',
+    answer:
+      'We use a variety of materials and resources, including textbooks, multimedia, and authentic materials, to keep our lessons engaging and dynamic. We also provide supplemental materials and resources to help you practice and reinforce what you learn in class.',
+  },
+  {
+    question: 'What are your scheduling options?',
+    answer:
+      'We offer flexible scheduling options to accommodate your busy lifestyle. We can schedule lessons at a time and frequency that works best for you, whether its once a week or several times a week.',
+  },
+  {
+    question: 'What if I need to cancel or reschedule a lesson?',
+    answer:
+      'We understand that life can be unpredictable, and we are happy to accommodate schedule changes when possible. Please let us know as soon as possible if you need to cancel or reschedule a lesson, and we will do our best to find a new time that works for you.',
+  },
+  {
+    question: 'How can I get in touch with you if I have any questions or concerns?',
+    answer:
+      'You can reach us by phone, email, or through the contact form on our website. We are always happy to answer any questions or concerns you may have about our courses or services.',
+  },
+];
+
+export default Faq;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import '../components/HomeStyle.css';
 import Navbar from './Navbar';
+import Typewriter from 'typewriter-effect';
+
 const Home = () => {
   
   return (
     <div className='home'>
     <Navbar/>
     
-    <div>Home</div>
+      <div className='Hero'>
+      <h1><Typewriter options={{
+        strings: ['नमस्ते','Hello', 'Sveiki', 'مرحبًا', 'Hola'],
+        autoStart: true,
+        loop: true, }} />Welcome to <span>Shabireh</span></h1>
+        <p>Dedicated to helping students achieve their language learning goals.<br/>
+        Let's start this language learning journey together!</p>
+        
+
+
+      </div>
     
     </div>
   )
