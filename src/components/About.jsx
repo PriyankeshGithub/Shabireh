ba921ef881c81f8e4a7219dde6d0195bf74ac4d5
PriyankeshGithub <priyankeshshivam@gmail.com>
2023-04-13 19:15:33 +0300

    Newmodification

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,12 @@ import '../components/AboutStyle.css';
 
 const About = () => {
   return (
-    <div className='about'>About</div>
+    <div className='about'>
+      <div className="info">
+      <h1 className='tittle'>Hello I`m Vesta</h1>
+      <p className='tittleDesc'>I am a freelance language teacher who teaches lithuanian, hindi and arabic.<br/> I study at Vilnius university where my major subject is India and South Asia, and my minor is Spanish language and culture</p>
+      </div>
+    </div>
   )
 }
 
