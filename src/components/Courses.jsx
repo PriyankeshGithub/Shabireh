2148329d73b4a0bbdeff5f52336f92486915d40b
PriyankeshGithub <priyankeshshivam@gmail.com>
2023-04-16 01:09:05 +0300

    uplolast

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -20,13 +20,14 @@ const Cards = (props) =>{
 }
 const Courses = () => {
   return (
+    <div className="c2">
     <div className='courses'>
     <h1 className='classesText'>Classes we offer</h1>
       <Cards img={lithLogo} title ="Lithuanian" desc="Lithuanian is a Baltic language spoken primarily in Lithuania and is one of the oldest living Indo-European languages. It is written in the Latin script and has retained many archaic features that have been lost in other Indo-European languages."/>
 
       <Cards img={hindiLogo} title ="Hindi" desc="Hindi is an Indo-Aryan language primarily spoken in India and is written in the Devanagari script. It is the fourth most spoken language in the world."/>
 
-      <Cards img={arabicLogo} title ="Arabic" desc="Arabic is a Semitic language primarily spoken in the Middle East and North Africa and is written in the Arabic script. It is the fifth most spoken language in the world and is also the liturgical language of Islam."/> </div>
+      <Cards img={arabicLogo} title ="Arabic" desc="Arabic is a Semitic language primarily spoken in the Middle East and North Africa and is written in the Arabic script. It is the fifth most spoken language in the world and is also the liturgical language of Islam."/> </div></div>
       )
   
 }
